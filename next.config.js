22b19e701ddf1c64053a932b72e55d0a6ad926f1
TaroHarado <TaroHarado@gmail.com>
2025-11-15 17:04:25 +0100

    Fix webpack warnings: Ignore optional dependencies for MetaMask SDK and pino-pretty

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack')
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Ignore optional dependencies that are not needed for web builds
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+      'pino-pretty': false,
+    }
+
+    // Ignore these modules during webpack bundling
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@react-native-async-storage\/async-storage$/,
+      }),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pino-pretty$/,
+      })
+    )
+
+    return config
+  },
 }
 
 module.exports = nextConfig
